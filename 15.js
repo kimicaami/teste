c165f0eb1181dc33eb0224e8b0c71f29064552fa
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-17 12:02:33 -0300

    Primeiro commit

---
+++ 15.js
@@ -0,0 +1,15 @@
+// Mostrar o maior entre 3 números
+
+let numero1 = 10
+let numero2 = 2
+let numero3 = 15
+
+if(numero1 >= numero2 && numero1 >= numero3){
+    console.log(numero1)
+}
+else if(numero2 >= numero3){
+    console.log(numero2)
+}
+else{
+    console.log(numero3)
+}
---
+++ if.py
@@ -0,0 +1,17 @@
+# Verificar se é maior de idade
+
+maiorIdade = 18
+idade = int(input('Digite a sua idade:\n'))
+
+if (idade >= maiorIdade):
+    print('Eh maior de idade.')
+else:
+    print('Eh menor de idade.')
+
+# if (variável 'Sinal' variável)
+# É igual/diferente?
+# Igual: ==
+# Diferente: !=
+# É maior/menor?
+# Maior: > (Maior ou igual: >=)
+# Menor: < (Menor ou igual: <=)
